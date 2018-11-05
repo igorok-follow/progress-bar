8e22c455f60c4660cb8f44bf24bf6d6d200e90d3
Igor Shchelchkov <38875305+igorok-follow@users.noreply.github.com>
2018-11-05 06:40:10 +0300

    Add files via upload
    First commit.

---
+++ Progress.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.awt.*;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+
+
+public class Progress extends JFrame {
+    JProgressBar current;
+    JTextArea out;
+    JButton find;
+    Thread runner;
+    int num = 0;
+
+
+    public Progress() {
+        super("Progress bar");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pane = new JPanel();
+        pane.setLayout(new FlowLayout());
+        current = new JProgressBar(0, 20000);
+        current.setValue(0);
+        current.setStringPainted(true);
+        pane.add(current);
+        setContentPane(pane);
+    }
+
+
+
+    public void iterate() {
+        while (num < 20000) {
+            current.setValue(num);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) { }
+            num += 95;
+        }
+    }
+
+
+    public static void main(String[] arguments) {
+        Progress frame = new Progress();
+        frame.pack();
+        frame.setVisible(true);
+        frame.iterate();
+    }
+
+}
